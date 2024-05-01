c1d25e3f0f4191bc802ab7eb63caf56f2bf4c113
5ily <146524790+LilpanXD@users.noreply.github.com>
2024-05-01 20:22:51 +0700

    Update Aimbot2.lua

--- Aimbot2.lua
+++ Aimbot2.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/x2-Neptune/AlchemyHub/main/Main.lua"))({
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LilpanXD/Bloxfruit/main/AimBot.lua"))({
     "discord.gg/LilpanXD","x2neptune.tech/LoaderPromo",true,"BY:LilpanXD"
 })
