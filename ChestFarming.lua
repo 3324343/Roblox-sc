64405d4cbdfcc532b5df91903d46e1f6496e775f
5ily <146524790+LilpanXD@users.noreply.github.com>
2024-05-12 02:49:27 +0700

    Update ChestFarming.lua

--- ChestFarming.lua
+++ ChestFarming.lua
@@ -91,20 +91,20 @@ end)
 local AllowRunService = true
 local AllowRunServiceBind = Instance.new("BindableFunction")
 function AllowRunServiceBind.OnInvoke(args)
-    if args == "on" then
+    if args == "ON" then
         AllowRunService = true
-    elseif args == "off" then
+    elseif args == "OFF" then
         AllowRunService = false
     end
     local CoreGui = game:GetService("StarterGui")
     CoreGui:SetCore("SendNotification", {
-        Title = "ChestFarm",
+        Title = "===[CHEST FARMING]===",
         Text = "Chest Farm By 5ily#0000",
         Duration = math.huge,
         Callback = AllowRunServiceBind,
-        Button1 = "on",
-        Button2 = "off",
-        Logo = "https://cdn.discordapp.com/avatars/1163562149415747615/cc281554a3c8b1040d8faa71725f385d?size=256"
+        Button1 = "ON",
+        Button2 = "OFF",
+        Logo = "https://cdn.discordapp.com/avatars/1163562149415747615/cc281554a3c8b1040d8faa71725f385d?size=256",
     })
 end
 
@@ -123,12 +123,13 @@ end)
 
 local CoreGui = game:GetService("StarterGui")
 CoreGui:SetCore("SendNotification", {
-    Title = "ChestFarm",
+    Title = "===[CHEST FARMING]===",
     Text = "Chest Farm By 5ily#0000",
     Duration = math.huge,
     Callback = AllowRunServiceBind,
-    Button1 = "on",
-    Button2 = "off",
+    Button1 = "ON",
+    Button2 = "OFF",
+    Logo = "https://cdn.discordapp.com/avatars/1163562149415747615/cc281554a3c8b1040d8faa71725f385d?size=256",
 })
 task.spawn(function()
     while true and task.wait(.5) do
